44dc71510045008efeac1da06c05d45a869f703e
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-08-15 13:23:14 +0200

    08.Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return "Home page";
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return "Contact";
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return 'Blog post '. $id;
+})->name('posts.show');
+
+Route::get('/recent-posts/{days_ago?}', function ($days_ago = 20) {
+    return 'Posts from '. $days_ago . ' days ago' ;
+})->name('posts.recent.index');
