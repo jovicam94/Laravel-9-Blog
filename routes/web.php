a2e4c1ca9278507b911c3b40f627116eeb281626
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-08-18 14:51:24 +0200

    Create header and cookie

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,9 @@ Route::get('/posts/{id}', function ($id) use ($posts) {
 Route::get('/recent-posts/{days_ago?}', function ($days_ago = 20) {
     return 'Posts from '. $days_ago . ' days ago' ;
 })->name('posts.recent.index');
+
+Route::get('/fun/responses', function () use($posts) {
+    return response($posts, 201)
+        ->header('Content-Type', 'application/json')
+        ->cookie('MY_COOKIE', 'Jovica Misirlic', 3600);
+});
