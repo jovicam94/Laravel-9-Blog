de57bd22bba387ff2e723fb5620870039357f469
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-08-19 14:47:51 +0200

    backend/15 Add create page with store functionality

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -25,7 +25,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -36,7 +36,12 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new BlogPost();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect()->route('posts.show', ['post' => $post->id]);
     }
 
     /**
