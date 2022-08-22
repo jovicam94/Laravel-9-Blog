2b5e3b2d57bfca90de3743f9c0a1af1b7ea955ab
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-08-22 13:33:19 +0200

    Add field validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
 
@@ -34,12 +35,16 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePost $request)
     {
+        $validated = $request->validated();
+
         $post = new BlogPost();
-        $post->title = $request->input('title');
-        $post->content = $request->input('content');
+        $post->title = $validated['title'];
+        $post->content = $validated['content'];
         $post->save();
+
+        $request->session()->flash('status', 'The blog post was created!');
 
         return redirect()->route('posts.show', ['post' => $post->id]);
     }
