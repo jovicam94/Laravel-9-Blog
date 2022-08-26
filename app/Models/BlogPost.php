bf42619c0a20d18e280ad22b650332e5cc669058
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-08-26 14:36:19 +0200

    Add one-to-many relationship with Comments model

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class BlogPost extends Model
 {
     protected $fillable = ['title', 'content'];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
     use HasFactory;
 }
