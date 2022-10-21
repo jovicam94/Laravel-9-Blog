75625de60edd1b6a145a9b80f873a832c82de7c2
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-10-21 15:25:15 +0200

    Add model factories

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogPost>
+ */
+class BlogPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(10),
+            'content' => fake()->paragraphs(5, true)
+        ];
+    }
+
+    public function new_title_content_state()
+    {
+        return $this->state(function () {
+            return [
+                'title' => 'Title of blog post',
+                'content' => 'Content of the blog post....'
+            ];
+        });
+    }
+}
