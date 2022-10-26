7b945f11776776cc20cff87b2dfa33f13622509e
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-10-26 14:46:04 +0200

    Add authorization on pages

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -5,4 +5,10 @@
 @section('content')
     <h1>Contact</h1>
     <p>Hello this is contact!</p>
+
+    @can('home.secret')
+        <a href="{{ route('secret') }}">
+        <p>Special contact details!</p>
+        </a>
+    @endcan
 @endsection
