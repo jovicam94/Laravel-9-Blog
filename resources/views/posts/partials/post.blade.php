015d7c8efb7c5b2d3620df1bec089adff80e8cda
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-11-23 15:23:44 +0100

    Add deleted posts to view and change delete button logics

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,6 +1,13 @@
+@if($post->trashed())
+    <del>
+@endif
 <h3>
-    <a class="text-decoration-none" href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
+    <a class="text-decoration-none {{ $post->trashed() ? 'text-muted' : '' }}" href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
 </h3>
+@if($post->trashed())
+    </del>
+@endif
+
 <p class="text-muted">
     Added {{ $post->created_at->diffForHumans() }}
     by {{ $post->user->name }}
@@ -19,11 +26,13 @@
 {{--    @cannot('delete', $post)--}}
 {{--        <p>You can't delete this post</p>--}}
 {{--    @endcannot--}}
-    @can('delete', $post)
-        <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}", method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="submit" value="Delete!" class="btn btn-primary">
-        </form>
-    @endcan
+    @if(!$post->trashed())
+        @can('delete', $post)
+            <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}", method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Delete!" class="btn btn-primary">
+            </form>
+        @endcan
+    @endif
 </div>
