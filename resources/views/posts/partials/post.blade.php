a3744e89d179e1237cc40c8e4f29e94d0ba78883
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-06 15:16:18 +0100

    Change partial view part to auth

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -23,13 +23,15 @@
 {{--    @cannot('delete', $post)--}}
 {{--        <p>You can't delete this post</p>--}}
 {{--    @endcannot--}}
-    @if(!$post->trashed())
-        @can('delete', $post)
-            <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}", method="POST">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Delete!" class="btn btn-primary">
-            </form>
-        @endcan
-    @endif
+    @auth
+        @if(!$post->trashed())
+            @can('delete', $post)
+                <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}", method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete!" class="btn btn-primary">
+                </form>
+            @endcan
+        @endif
+    @endauth
 </div>
