fa6b90a253eeab27a393dbfba42ca0cea9c02138
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-15 14:22:22 +0100

    Add fillable and creating action for Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,16 +6,26 @@ use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
 
 class Comment extends Model
 {
 
 
     use SoftDeletes;
+
+    protected $fillable = ['user_id', 'content'];
+
+
     // blog_post_id
     public function blog_post()
     {
         return $this->belongsTo('App\Models\BlogPost');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
     use HasFactory;
@@ -23,6 +33,11 @@ class Comment extends Model
     public static function boot()
     {
         parent::boot();
+
+        static::creating(function (Comment $comment) {
+            Cache::tags(['blog-post'])->forget("blog-post-{$comment->blog_post_id}");
+            Cache::tags(['blog-post'])->forget("mostCommented");
+        });
 
 //        static::addGlobalScope(new LatestScope);
 
