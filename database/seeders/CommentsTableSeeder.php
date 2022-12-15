34317f02416c05b3868484bebe97d32259a50e91
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-15 14:24:54 +0100

    Update CommentsSeeder for creating comments by random users

--- database/seeders/CommentsTableSeeder.php
+++ database/seeders/CommentsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\BlogPost;
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -27,8 +28,11 @@ class CommentsTableSeeder extends Seeder
 
         $commentsCount = (int)$this->command->ask('How many comments would you like?', 150);
 
-        Comment::factory($commentsCount)->make()->each(function ($comment) use ($posts) {
+        $users = User::all();
+
+        Comment::factory($commentsCount)->make()->each(function ($comment) use ($posts, $users) {
             $comment->blog_post_id = $posts->random()->id;
+            $comment->user_id = $users->random()->id;
             $comment->save();
         });
     }
