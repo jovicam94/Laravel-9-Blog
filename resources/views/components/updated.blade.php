39dc9e59bf177c98d2cee5688b7f0fa980813820
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-21 13:57:23 +0100

    Linking users to their profiles

--- resources/views/components/updated.blade.php
+++ resources/views/components/updated.blade.php
@@ -1,6 +1,10 @@
 <p class="text-muted">
     {{ $type ?? "Added " }} {{ $date->diffForHumans() }}
     @if(isset($name))
-        by {{ $name }}
+        @if(isset($user))
+            by <a class="text-decoration-none" href="{{ route('users.show', ['user' => $user]) }}">{{ $name }}</a>
+        @else
+            by {{ $name }}
+        @endif
     @endif
 </p>
