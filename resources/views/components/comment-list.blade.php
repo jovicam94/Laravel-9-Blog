707f4eca0ecd20817e9ca90681bb471626f04f7f
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-22 14:40:38 +0100

    Make visible comment tags

--- resources/views/components/comment-list.blade.php
+++ resources/views/components/comment-list.blade.php
@@ -3,6 +3,7 @@
         {{ $comment->content }}
     </p>
     <x-updated :date="$comment->created_at" :name="$comment->user->name" :user="$comment->user->id" />
+    <x-tags :tags="$comment->tags" />
 @empty
     <p>No comments yet!</p>
 @endforelse
