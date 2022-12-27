a57ee04299bf24a909600d011b6476809cc19707
Jovica Misirlic <jovica.misirlic@gmail.com>
2022-12-27 09:47:49 +0100

    Add mail class and send mail notification for commenting post

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComment;
+use App\Mail\CommentPosted;
+use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
@@ -16,10 +19,14 @@ class PostCommentController extends Controller
 
     public function store(BlogPost $post, StoreComment $request)
     {
-        $post->comments()->create([
+        $comment = $post->comments()->create([
             'content' => $request->input('content'),
             'user_id' => $request->user()->id
         ]);
+
+        Mail::to($post->user)->send(
+            new CommentPostedMarkdown($comment)
+        );
 
         $request->session()->flash('create', 'Comment was created.');
 
