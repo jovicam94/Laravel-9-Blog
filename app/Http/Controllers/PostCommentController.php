92d52ca618e12c732771142dc7c01cb750eb0bb7
Jovica Misirlic <jovica.misirlic@gmail.com>
2023-01-16 12:13:19 +0100

    Use resource for response in controller

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -7,6 +7,7 @@ use App\Models\BlogPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Events\CommentPosted;
+use App\Http\Resources\Comment as CommentResource;
 
 class PostCommentController extends Controller
 {
@@ -15,12 +16,17 @@ class PostCommentController extends Controller
         $this->middleware('auth')->only(['store']);
     }
 
+    public function index(BlogPost $post)
+    {
+        return CommentResource::collection($post->comments()->with('user')->get());
+//        return $post->comments()->with('user')->get();
+    }
+
 
     public function store(BlogPost $post, StoreComment $request)
     {
         $comment = $post->comments()->create([
             'content' => $request->input('content'),
-            'user_id' => $request->user()->id
         ]);
 
         event(new CommentPosted($comment));
